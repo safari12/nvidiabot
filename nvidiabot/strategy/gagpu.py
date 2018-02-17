2acec12925deca70397c8fafe26fd87c27f178a8
safari <rsafari.s@gmail.com>
2018-02-17 03:19:24 -0600

    web scraped nvidia gpu product header

--- nvidiabot/strategy/gagpu.py
+++ nvidiabot/strategy/gagpu.py
@@ -1,3 +1,6 @@
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+
 from nvidiabot.strategy import BaseStrategy
 
 
@@ -23,3 +26,38 @@ class GAGPU(BaseStrategy):
 
     def run(self):
         print("Emails " + ', '.join(self.emails))
+
+        self.are_gpus_in_stock()
+
+    def are_gpus_in_stock(self):
+        nvidia_url = 'https://www.nvidia.com/en-us/geforce/products/10series/geforce-store/'
+        nvidia_html = urlopen(nvidia_url)
+
+        nvidia_soap = BeautifulSoup(nvidia_html, 'html.parser')
+
+        nvidia_section_box = nvidia_soap.find('div', attrs={
+            'id': 'section-2'
+        })
+
+        nvidia_product_divs = nvidia_section_box.div.find_all('div', recursive=False)
+
+        for d in nvidia_product_divs:
+            product_name = d.h2.text.strip()
+
+            print(d.div)
+
+        # for product_col in nvidia_section_box.div.children:
+        #     print(product_col['class'])
+
+        # nvidia_section_cols = nvidia_section_box.div.find_next_siblings('div')
+        #
+        # print(len(nvidia_section_cols))
+
+        # print(nvidia_section_cols[0].find(attrs={
+        #     'class': 'product-heading1'
+        # }).text.strip())
+
+        # print(nvidia_section_cols[0].h2)
+
+        # for nvs_col in nvidia_section_cols:
+        #     print(nvs_col['class'])
